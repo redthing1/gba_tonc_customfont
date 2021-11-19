9262db08ac6603ec823270ec187b8b4b8d520dd5
redthing1 <xhyinne@gmail.com>
2021-11-18 20:54:43 -0800

    add custom font stub

---
+++ src/amstrad2y_gly2.h
@@ -0,0 +1,20 @@
+
+//{{BLOCK(amstrad2y_gly2)
+
+#ifndef __AMSTRAD2Y_GLY2__
+#define __AMSTRAD2Y_GLY2__
+
+extern const TFont amstrad2y_gly2Font;
+
+#define amstrad2y_gly2GlyphsLen 32768
+extern const unsigned int amstrad2y_gly2Glyphs[8192];
+
+#define amstrad2y_gly2WidthsLen 128
+extern const unsigned char amstrad2y_gly2Widths[128];
+
+#define amstrad2y_gly2PalLen 32
+extern const unsigned short amstrad2y_gly2Pal[32];
+
+#endif // __AMSTRAD2Y_GLY2__
+
+//}}BLOCK(amstrad2y_gly2)
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "tonc.h"
+#include "amstrad2y_gly2.h"
 
 int main() {
     // init
@@ -7,7 +8,8 @@ int main() {
     pal_bg_mem[0] = 0x0C02; // background color
 
     // init tte
-    tte_init_chr4c(0, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_WHITE, &verdana9Font, NULL);
+    // tte_init_chr4c(0, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_WHITE, &verdana9Font, NULL);
+    tte_init_chr4c(0, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_WHITE, &amstrad2y_gly2Font, NULL);
 
     tte_init_con();
     tte_printf("#{P:0,0}abcdefghijklm");
